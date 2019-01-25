e2689e301d812c1e699509af9f6e850f6e20c80f
akhil-accolite <46513494+akhil-accolite@users.noreply.github.com>
2019-01-25 09:44:40 +0530

    female correction

--- DesignPattern/AbstractFactory/src/com/testPattern/testPattern.java
+++ DesignPattern/AbstractFactory/src/com/testPattern/testPattern.java
@@ -1,5 +1,6 @@
 package com.testPattern;
 
+import com.abstractFactory.FemaleFactory;
 import com.abstractFactory.HumanFactory;
 import com.abstractFactory.MaleFactory;
 import com.model.Human;
@@ -11,7 +12,7 @@ public class testPattern {
 	private static void testAbstractFactory() {
 
 		Human male = HumanFactory.recognizeHuman(new MaleFactory("short", "long", "more"));
-		Human female = HumanFactory.recognizeHuman(new MaleFactory("long", "short", "less"));
+		Human female = HumanFactory.recognizeHuman(new FemaleFactory("long", "short", "less"));
 
 		System.out.println("recognized male: " + male.toString());
 		System.out.println("recognized female: " + female.toString());
